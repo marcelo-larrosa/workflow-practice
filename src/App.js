8d87af6ddb49859085b06735e7bddd52915ed285
Marcelo <marcelo@muustack.com>
2022-01-08 00:28:00 -0300

    conflicto titulo resuelto

--- src/App.js
+++ src/App.js
@@ -5,8 +5,9 @@ export function App(){
     return(
         <>
             <header>
-                <h1>Peliculas</h1>
+                <h1>Ultimas Peliculas</h1>
             </header>
+
             <main>
                 <MovieGrid></MovieGrid>
             </main>
